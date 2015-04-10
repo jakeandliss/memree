7ecde855a7226eb43c13bb09f4434ed75a22d251
Satendra <khangar.satendra@gmail.com>
2015-04-10 18:25:56 +0530

    display images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150409213633) do
+ActiveRecord::Schema.define(version: 20150409131117) do
 
   create_table "entries", force: :cascade do |t|
     t.string   "entry"
