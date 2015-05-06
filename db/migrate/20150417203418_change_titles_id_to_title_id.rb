227cb3a8763ba7f00d6eec42bd680c00f12e9e16
efremov <efremov.artserg@gmail.com>
2015-05-06 04:51:57 +0300

    Rails + Ancestry Gem for Tags

--- db/migrate/20150417203418_change_titles_id_to_title_id.rb
+++ db/migrate/20150417203418_change_titles_id_to_title_id.rb
@@ -1,6 +1,6 @@
 class ChangeTitlesIdToTitleId < ActiveRecord::Migration
   def change
-  	#remove_column :entries, :titles_id, :integer
-  	#add_column :entries, :title_id, :integer
+  	remove_column :entries, :titles_id, :integer
+  	add_column :entries, :title_id, :integer
   end
 end
