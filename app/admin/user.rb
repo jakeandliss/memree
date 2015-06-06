8a5911f6b5545ac333fd30514700d982ad28a6c6
Jake Thompson <jlcompany23@gmail.com>
2015-06-06 11:41:20 -0600

    fixed edit user in active admin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :first_name, :last_name, :email, :created_at, :last_sign_in_at, :last_sign_in_ip
+permit_params :first_name, :last_name, :email, :created_at, :last_sign_in_at, :last_sign_in_ip
 #
 # or
 #
@@ -19,10 +19,30 @@ index do
     column :first_name
     column :last_name
     column :email
+    column 'Entries', sortable: :entries_count do |c|
+        c.entries.count
+    end
+     column 'Last Entry', sortable: :entries_date do |c|
+        t = c.entries.first
+        unless t.nil?
+            t.title_date.to_date.strftime('%b, %e, %Y')
+    end
+    end
     column :created_at
     column :last_sign_in_at
     column :sign_in_count
     actions
+  end
+
+  form do |f|
+    f.inputs "User Details" do
+      f.input :first_name
+      f.input :last_name
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
   end
 
 end
