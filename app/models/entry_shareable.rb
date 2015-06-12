3b7d6c0e0cf27e47c26626a82ed6bf424b6d4b3b
Tajinder Bhullar <tajinder.fzr@gmail.com>
2015-06-12 22:07:41 +0530

    Quick fix for having shared tag for User2 only

--- app/models/entry_shareable.rb
+++ app/models/entry_shareable.rb
@@ -6,10 +6,7 @@ class EntryShareable < ActiveRecord::Base
 
 	private
 	def create_share_tag
-		users = [entry.user, user]
-		users.each do |u| 
-			tag = u.tags.where(name: "Shared").first_or_create!
+			tag = user.tags.where(name: "Shared").first_or_create!
 			entry.taggings.create(tag: tag)
-		end
 	end
 end
