20984147cfe86cf1be7451cae9170e2416e35aef
Jake Thompson <jlcompany23@gmail.com>
2015-06-23 12:06:30 -0600

    changed workers in delayed job initializer

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,7 +1,7 @@
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
 
 if(!File.exists?(Rails.root.join('tmp','pids', 'delayed_job.pid')))
-    system "RAILS_ENV=production ./bin/delayed_job start -n 5 &"
+    system "RAILS_ENV=production ./bin/delayed_job start -n 2 &
     system "./bin/delayed_job start &"
 else
 	system "echo \"delayed_jobs is running\""
