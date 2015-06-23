ce3cec2681b7903a47a2ea9a7c50bd15a6949ff1
Jake Thompson <jlcompany23@gmail.com>
2015-06-23 11:41:00 -0600

    2 workers for delayed jobs. changed to 3 seconds for thumbnail

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,7 +1,7 @@
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
 
 if(!File.exists?(Rails.root.join('tmp','pids', 'delayed_job.pid')))
-    system "echo \"Starting delayed_jobs...\""
+    system "RAILS_ENV=production ./bin/delayed_job start -n 5 &"
     system "./bin/delayed_job start &"
 else
 	system "echo \"delayed_jobs is running\""
